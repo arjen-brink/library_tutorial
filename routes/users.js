9c55bd8d25c774bf3481a13b8b79c77beb0a6457
Arjen Brink <arjen@digital-strategies.nl>
2018-09-08 17:59:11 +0200

    Added mongoose db

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Test
+router.get('/cool/', function(req, res, next) {
+  res.send('respond with cool');
+});
+
 module.exports = router;
